9577900cc2ee1136484b72862438368975303d13
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2025-03-20 15:12:07 +0100

    Removed SERVER, added schema env vars

--- tests/testthat/test-database.R
+++ tests/testthat/test-database.R
@@ -13,14 +13,13 @@ if (dir.exists(Sys.getenv("DATABASECONNECTOR_JAR_FOLDER"))) {
 }
 
 test_that("Snowflake", {
-  skip_if(Sys.getenv("SNOWFLAKE_SERVER") == "")
+  skip_if(Sys.getenv("SNOWFLAKE_CONNECTION_STRING") == "")
   print(sprintf(">> %s", Sys.getenv("DATABASE")))
 
   connectionDetails <- DatabaseConnector::createConnectionDetails(
     dbms = Sys.getenv("DATABASE"),
     user = Sys.getenv("SNOWFLAKE_USER"),
     password = Sys.getenv("SNOWFLAKE_PASSWORD"),
-    server = Sys.getenv("SNOWFLAKE_SERVER"),
     connectionString = Sys.getenv("SNOWFLAKE_CONNECTION_STRING"),
     pathToDriver = jdbcDriverFolder
   )
@@ -28,8 +27,8 @@ test_that("Snowflake", {
   ## new() ----
   cdmInterface <- TreatmentPatterns:::CDMInterface$new(
     connectionDetails = connectionDetails,
-    cdmSchema = "main",
-    resultSchema = "main"
+    cdmSchema = Sys.getenv("SNOWFLAKE_CDM_SCHEMA"),
+    resultSchema = Sys.getenv("SNOWFLAKE_RESULT_SCHEMA")
   )
 
   expect_true(R6::is.R6(
