affab8a791324f44d88cbb7e00de2b149bc7ea5d
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2025-03-21 12:30:57 +0100

    Updated cohort creation query

--- tests/testthat/test-database.R
+++ tests/testthat/test-database.R
@@ -82,13 +82,16 @@ test_that("Test Database", {
     sql = "
     DROP TABLE IF EXISTS @resultSchema.@cohortTableName;
 
-    SELECT TOP 10
-      1 AS cohort_definition_id,
-      person_id AS subject_id,
-      observation_period_start_date AS cohort_start_date,
-      observation_period_end_date AS cohort_end_date
+    SELECT *
     INTO @resultSchema.@cohortTableName
-    FROM @cdmSchema.observation_period;",
+    FROM (
+      SELECT TOP 10
+        1 AS cohort_definition_id,
+        person_id AS subject_id,
+        observation_period_start_date AS cohort_start_date,
+        observation_period_end_date AS cohort_end_date
+      FROM @cdmSchema.observation_period
+    ) a;",
     cdmSchema = CDM_SCHEMA,
     resultSchema = RESULT_SCHEMA,
     cohortTableName = cohortTableName
