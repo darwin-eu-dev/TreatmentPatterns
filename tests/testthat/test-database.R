55a415ec4c97d1f622cf398fa0887849a3e33ebb
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2025-03-21 11:58:57 +0100

    Changed LIMIT to TOP

--- tests/testthat/test-database.R
+++ tests/testthat/test-database.R
@@ -82,14 +82,13 @@ test_that("Test Database", {
     sql = "
     DROP TABLE IF EXISTS @resultSchema.@cohortTableName;
 
-    (SELECT
+    SELECT TOP 10
       1 AS cohort_definition_id,
       person_id AS subject_id,
       observation_period_start_date AS cohort_start_date,
       observation_period_end_date AS cohort_end_date
     INTO @resultSchema.@cohortTableName
-    FROM @cdmSchema.observation_period
-    LIMIT 10) a;",
+    FROM @cdmSchema.observation_period;",
     cdmSchema = CDM_SCHEMA,
     resultSchema = RESULT_SCHEMA,
     cohortTableName = cohortTableName
