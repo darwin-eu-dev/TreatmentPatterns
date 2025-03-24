b65c359a0033fa2ac265da8ca8d28a4d4407955f
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2025-03-24 14:15:21 +0100

    Updated select query

--- inst/sql/selectData.sql
+++ inst/sql/selectData.sql
@@ -1,26 +1,25 @@
 SELECT
-  @resultSchema.@cohortTable.cohort_definition_id,
-  @resultSchema.@cohortTable.subject_id,
-  @resultSchema.@cohortTable.cohort_start_date,
-  @resultSchema.@cohortTable.cohort_end_date,
-  YEAR(@resultSchema.@cohortTable.cohort_start_date) - @cdmSchema.person.year_of_birth AS age,
-  @cdmSchema.concept.concept_name AS sex,
+  @cohortTable.cohort_definition_id,
+  @cohortTable.subject_id,
+  @cohortTable.cohort_start_date,
+  @cohortTable.cohort_end_date,
+  YEAR(@cohortTable.cohort_start_date) - person.year_of_birth AS age,
+  concept.concept_name AS sex,
   subject_id_origin
 FROM
   @resultSchema.@cohortTable
 INNER JOIN @cdmSchema.person
-  ON @resultSchema.@cohortTable.subject_id = @cdmSchema.person.person_id
+  ON @cohortTable.subject_id = person.person_id
 INNER JOIN @cdmSchema.concept
-  ON @cdmSchema.person.gender_concept_id = @cdmSchema.concept.concept_id
+  ON person.gender_concept_id = concept.concept_id
 INNER JOIN
   (
     SELECT
-      ROW_NUMBER() OVER (PARTITION BY subject_id ORDER BY subject_id) AS subject_id,
+      ROW_NUMBER() OVER (ORDER BY subject_id) AS subject_id,
       CAST(subject_id AS VARCHAR) AS subject_id_origin
     FROM @resultSchema.@cohortTable
-    GROUP BY subject_id
   ) org_subject_table
-  ON CAST(subject_id_origin AS VARCHAR) = CAST(@resultSchema.@cohortTable.subject_id AS VARCHAR)
+  ON CAST(subject_id_origin AS VARCHAR) = CAST(@cohortTable.subject_id AS VARCHAR)
 WHERE
   cohort_definition_id IN (@cohortIds)
   AND DATEDIFF(d, cohort_start_date, cohort_end_date) >= @minEraDuration
