e471421d3247c2863087d9f776b0859266169368
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2025-03-26 15:44:54 +0100

    Optimized queries

--- inst/sql/selectData.sql
+++ inst/sql/selectData.sql
@@ -1,25 +1,28 @@
 SELECT
-  @cohortTable.cohort_definition_id,
-  @cohortTable.subject_id,
-  @cohortTable.cohort_start_date,
-  @cohortTable.cohort_end_date,
-  YEAR(@cohortTable.cohort_start_date) - person.year_of_birth AS age,
-  concept.concept_name AS sex,
-  subject_id_origin
-FROM
-  @resultSchema.@cohortTable
-INNER JOIN @cdmSchema.person
-  ON @cohortTable.subject_id = person.person_id
-INNER JOIN @cdmSchema.concept
-  ON person.gender_concept_id = concept.concept_id
-INNER JOIN
-  (
+    @cohortTable.cohort_definition_id,
+    new_id AS subject_id,
+    @cohortTable.cohort_start_date,
+    @cohortTable.cohort_end_date,
+    CAST(subject_id_origin AS VARCHAR) AS subject_id_origin,
+    YEAR(@cohortTable.cohort_start_date) - person.year_of_birth AS age,
+    concept.concept_name AS sex
+  FROM @resultSchema.@cohortTable
+  INNER JOIN (
     SELECT
-      ROW_NUMBER() OVER (ORDER BY subject_id) AS subject_id,
-      CAST(subject_id AS VARCHAR) AS subject_id_origin
-    FROM @resultSchema.@cohortTable
-  ) org_subject_table
-  ON CAST(subject_id_origin AS VARCHAR) = CAST(@cohortTable.subject_id AS VARCHAR)
-WHERE
-  cohort_definition_id IN (@cohortIds)
-  AND DATEDIFF(d, cohort_start_date, cohort_end_date) >= @minEraDuration
+      ROW_NUMBER() OVER (ORDER BY subject_id) AS new_id,
+      subject_id AS subject_id_origin
+    FROM (
+      SELECT
+  	    DISTINCT @cohortTable.subject_id
+      FROM
+        @resultSchema.@cohortTable
+    ) tmp
+  )
+    ON @cohortTable.subject_id = subject_id_origin
+  INNER JOIN @cdmSchema.person
+    ON subject_id_origin = person.person_id
+  INNER JOIN @cdmSchema.concept
+    ON person.gender_concept_id = concept.concept_id
+  WHERE
+    @cohortTable.cohort_definition_id IN (@cohortIds)
+    AND DATEDIFF(d, cohort_start_date, cohort_end_date) >= @minEraDuration
