66cbabf0a0ec483a1a06b8a8cdeb00a16a29c2ad
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2025-09-18 14:10:50 +0200

    Updated setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,10 @@ if (Sys.getenv("EUNOMIA_DATA_FOLDER", "") == "") {
   Sys.setenv("EUNOMIA_DATA_FOLDER" = tempfile("eunomiaData"))
   dir.create(Sys.getenv("EUNOMIA_DATA_FOLDER"))
   
+  if (Sys.getenv("EUNOMIA_DATA_FOLDER") |> list.files() |> length() == 0) {
+    Eunomia::downloadEunomiaData("GiBleed")
+  }
+
   withr::defer(
     {
       unlink(Sys.getenv("EUNOMIA_DATA_FOLDER"), recursive = TRUE, force = TRUE)
