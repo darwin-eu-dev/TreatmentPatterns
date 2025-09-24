ad99ca57826666f2d22ec109a0072fe89dece83c
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2025-09-24 15:42:57 +0200

    Fixed tests and updated setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,14 +1,29 @@
-if (Sys.getenv("EUNOMIA_DATA_FOLDER", "") == "") {
-  Sys.setenv("EUNOMIA_DATA_FOLDER" = tempfile("eunomiaData"))
-  dir.create(Sys.getenv("EUNOMIA_DATA_FOLDER"))
+# if (Sys.getenv("EUNOMIA_DATA_FOLDER") == "") {
+#   Sys.setenv("EUNOMIA_DATA_FOLDER" = tempfile("eunomiaData"))
+#   dir.create(Sys.getenv("EUNOMIA_DATA_FOLDER"))
+# 
+#   if (require("CDMConnector", quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)) {
+#     CDMConnector::downloadEunomiaData()
+#   }
+# 
+#   if (require("Eunomia", quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)) {
+#     Eunomia::downloadEunomiaData(datasetName = "GiBleed")
+#   }
+# 
+#   if (require("withr", quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)) {
+#     withr::defer(
+#     {
+#       unlink(Sys.getenv("EUNOMIA_DATA_FOLDER"), recursive = TRUE, force = TRUE)
+#     },
+#     if (require("testthat", quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)) {
+#       testthat::teardown_env()
+#     }
+#   )
+#   }
+# }
 
-  CDMConnector::downloadEunomiaData()
-  Eunomia::downloadEunomiaData(datasetName = "GiBleed")
-
-  withr::defer(
-    {
-      unlink(Sys.getenv("EUNOMIA_DATA_FOLDER"), recursive = TRUE, force = TRUE)
-    },
-    testthat::teardown_env()
-  )
-}
+withr::local_envvar(
+  R_USER_CACHE_DIR = tempfile(),
+  .local_envir = teardown_env(),
+  EUNOMIA_DATA_FOLDER = Sys.getenv("EUNOMIA_DATA_FOLDER", unset = tempfile())
+)
