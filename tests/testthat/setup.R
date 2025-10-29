60a963b2ad0c9ed9c7adb18ea95f749c8b6a03f2
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2025-10-29 13:50:37 +0100

    Updated setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,5 @@
+require("withr", quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)
+
 if (Sys.getenv("EUNOMIA_DATA_FOLDER", "") == "") {
   Sys.setenv("EUNOMIA_DATA_FOLDER" = tempfile("eunomiaData"))
   dir.create(Sys.getenv("EUNOMIA_DATA_FOLDER"))
@@ -13,7 +15,6 @@ if (Sys.getenv("EUNOMIA_DATA_FOLDER", "") == "") {
   withr::defer(
     {
       unlink(Sys.getenv("EUNOMIA_DATA_FOLDER"), recursive = TRUE, force = TRUE)
-    },
-    testthat::teardown_env()
+    }
   )
 }
