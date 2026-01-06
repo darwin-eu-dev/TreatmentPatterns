e013a93cea01e345ce7b158bca76c3c076e7cbc5
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-01-06 15:13:37 +0100

    updated nrow for updated attrition

--- tests/testthat/test-exportPatientLevel.R
+++ tests/testthat/test-exportPatientLevel.R
@@ -47,6 +47,6 @@ test_that("exportPatientLevel", {
   expect_equal(ncol(attrition), 5)
   expect_equal(nrow(attrition), 11)
 
-  expect_equal(ncol(cdm_source_info), 10)
+  expect_equal(ncol(cdm_source_info), 11)
   expect_equal(nrow(cdm_source_info), 1)
 })
