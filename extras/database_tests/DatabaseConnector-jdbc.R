66524050a2ff1de4db4a52fe100999b3e9643a1c
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-01-26 12:16:23 +0100

    postgres and oracle fix

--- extras/database_tests/DatabaseConnector-jdbc.R
+++ extras/database_tests/DatabaseConnector-jdbc.R
@@ -71,7 +71,8 @@ test_that("Test Database", {
   cdm <- CDMConnector::cdmFromCon(
     con = connection,
     cdmSchema = Sys.getenv("CDM_SCHEMA"),
-    writeSchema = Sys.getenv("RESULT_SCHEMA")
+    writeSchema = Sys.getenv("RESULT_SCHEMA"),
+    cdmVersion = "5.4"
   )
 
   ## Prepare ----
