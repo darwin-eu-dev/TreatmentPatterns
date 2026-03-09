980de46cb1dc640bd839ccc5972e52441d0422ba
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-03-09 12:00:05 +0100

    layer 1 order set and fixed strata

--- R/ggSunburst2.R
+++ R/ggSunburst2.R
@@ -1,23 +1,16 @@
-# library(ggplot2)
-# library(dplyr)
-
-# d <- mergeIndividualPathways(df)
-# 
-# d |>
-#   group_by(.data$layer_1) |>
-#   arrange(.data$freq)
-
-mergeIndividualPathways <- function(treatmentPathways) {
-  layerOneTotal <- sum(treatmentPathways$freq)
+mergeIndividualPathways <- function(treatmentPathways, strataX, strataY) {
+  # layerOneTotal <- sum(treatmentPathways$freq)
   maxLayer <- max(sapply(strsplit(treatmentPathways$pathway, split = "-"), length))
-  
+
   layerColumns <- sprintf("layer_%s", 1:maxLayer)
   
   naReplaceList <- as.list(rep("", maxLayer))
   names(naReplaceList) <- layerColumns
   
   dat <- treatmentPathways |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY))) |>
     dplyr::mutate(
+      total = sum(.data$freq),
       path_to_sep = .data$pathway,
       path_id = dplyr::row_number()
     ) |>
@@ -31,22 +24,23 @@ mergeIndividualPathways <- function(treatmentPathways) {
     dplyr::group_by(.data$layer_1) |>
     dplyr::mutate(l1_freq = sum(.data$freq)) |>
     dplyr::ungroup() |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY))) |>
     dplyr::arrange(.data$l1_freq, !!!rlang::parse_exprs(layerColumns)) |>
     dplyr::mutate(
-      frac = .data$freq / layerOneTotal * 100,
+      frac = .data$freq / .data$total * 100,
       xmax = cumsum(.data$frac),
       xmin = .data$xmax - .data$frac
     ) |>
     tidyr::separate_longer_delim(cols = "pathway", delim = "-") |>
     dplyr::rename(event = "pathway") |>
-    dplyr::group_by(.data$path_id) |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), .data$path_id) |>
     dplyr::mutate(layer = dplyr::row_number()) |>
     dplyr::ungroup()
   
   lapply(1:maxLayer, function(i) {
     layerDat <- dat |>
       dplyr::filter(.data$layer == i) |>
-      dplyr::group_by(!!!rlang::parse_exprs(layerColumns[1:i])) |>
+      dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), !!!rlang::parse_exprs(layerColumns[1:i])) |>
       dplyr::reframe(
         layer = .data$layer,
         event = .data$event,
@@ -60,7 +54,7 @@ mergeIndividualPathways <- function(treatmentPathways) {
     dplyr::bind_rows()
 }
 
-splitCombinations <- function(treatmentPathways) {
+splitCombinations <- function(treatmentPathways, strataX, strataY) {
   layerCols <- names(treatmentPathways)[grepl(pattern = "^layer_\\d$", names(treatmentPathways))]
   
   n <- sum(grepl(names(treatmentPathways), pattern = "^layer_\\d$"))
@@ -70,7 +64,7 @@ splitCombinations <- function(treatmentPathways) {
       event_to_split = .data$event
     ) |>
     tidyr::separate_longer_delim(cols = "event_to_split", delim = "+") |>
-    dplyr::group_by(!!!rlang::parse_exprs(layerCols)) |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), !!!rlang::parse_exprs(layerCols)) |>
     dplyr::mutate(
       comb_id = dplyr::row_number(),
       comb_max = max(dplyr::row_number())
@@ -86,75 +80,58 @@ splitCombinations <- function(treatmentPathways) {
     )
 }
 
-plotSunburst <- function(treatmentPathways) {
-  ggDat <- mergeIndividualPathways(treatmentPathways) |>
-    splitCombinations()
-  
-  gg <- ggplot(data = ggDat)
+plotSunburst <- function(treatmentPathways, strataX, strataY) {
+  ggDat <- mergeIndividualPathways(treatmentPathways, strataX, strataY) |>
+    splitCombinations(strataX, strataY)
+
+  gg <- ggplot2::ggplot(data = ggDat)
   
   nLayers <- sum(grepl(pattern = "^layer_\\d$", names(ggDat)))
   
   for (i in 1:nLayers)
-    gg <- gg + geom_rect(
+    gg <- gg + ggplot2::geom_rect(
       data = ggDat |>
         dplyr::filter(.data$layer == i),
-      mapping = aes(
-        ymin = ymin,
-        ymax = ymax,
-        xmin = xmin,
-        xmax = xmax,
-        fill = event
+      mapping = ggplot2::aes(
+        ymin = .data$ymin,
+        ymax = .data$ymax,
+        xmin = .data$xmin,
+        xmax = .data$xmax,
+        fill = .data$event
       ),
       colour = "#000000"
     )
-  
+
   gg +
-    coord_polar() +
-    theme(
+    ggplot2::coord_polar() +
+    ggplot2::theme(
       axis.text.y = ggplot2::element_blank(),
       axis.ticks = ggplot2::element_blank()
     ) +
-    ylim(0, nLayers + 1) +
-    theme_void()
+    ggplot2::ylim(0, nLayers + 1) +
+    ggplot2::facet_grid(
+      rows = vars(!!!rlang::parse_exprs(strataY)),
+      cols = vars(!!!rlang::parse_exprs(strataX))
+    )
 }
 
-ggPlotSunburst2 <- function(treatmentPathways, minFreq = 0, strataX = NULL, strataY = NULL) {
-  nAge <- length(unique(treatmentPathways$age))
-  nSex <- length(unique(treatmentPathways$sex))
-  nYear <- length(unique(treatmentPathways$index_year))
-  
-  colMulGroups <- c("age", "sex", "index_year")[as.logical((c(nAge, nSex, nYear) - 1))]
-  colOneGroups <- c("age", "sex", "index_year")[!as.logical((c(nAge, nSex, nYear) - 1))]
-  
-  gg <- if (sum(nAge, nSex, nYear) == 3) {
-    plotSunburst(treatmentPathways)
-  } else if (length(colMulGroups) == 1) {
-    filterDf <- lapply(colMulGroups, function(col) {
-      treatmentPathways |>
-        dplyr::summarise(
-          !!rlang::sym(col) := min(.data[[col]])
-        )
-    }) |>
-      dplyr::bind_cols()
-    
-    filterDf <- lapply(colOneGroups, function(col) {
-      dplyr::tibble(!!rlang::sym(col) := "all")
-    }) |>
-      dplyr::bind_cols() |>
-      dplyr::bind_cols(filterDf)
-    
-    warning(
-      sprintf(
-        "Multiple groups detected for columns: %s. Defaulting to: `%s`",
-        paste(colMulGroups, collapse = ", "),
-        paste(sprintf("%s == '%s'", colMulGroups, filterDf[colMulGroups]), collapse = ", ")
-      )
-    )
-    
-    treatmentPathways |>
-      dplyr::right_join(filterDf) |>
-      plotSunburst()
+ggPlotSunburst2 <- function(treatmentPathways, minFreq = 0, strataX = "", strataY = "", style = "default") {
+  gg <- treatmentPathways |>
+    dplyr::filter(.data$freq >= minFreq) |>
+    plotSunburst(strataX, strataY)
+
+  if (minFreq >= 0) {
+    nPaths <- treatmentPathways |>
+      dplyr::filter(.data$freq < minFreq) |>
+      nrow()
+    message(sprintf("Filtered out %s pathways with a frequency < %s", nPaths, minFreq))
   }
+
+  if (style == "darwin") {
+    gg <- gg +
+      visOmopResults::themeVisOmop(style = "darwin")
+  }
+
   return(gg)
 }
 
@@ -166,12 +143,10 @@ ggPlotSunburst2 <- function(treatmentPathways, minFreq = 0, strataX = NULL, stra
 # )
 # 
 # df <- tpr$treatment_pathways |>
-#   dplyr::filter(.data$freq >= 20) |>
-#   dplyr::mutate(sex = "male") |>
+#   dplyr::mutate(database = "foo") |>
 #   dplyr::bind_rows(
 #     tpr$treatment_pathways |>
-#       dplyr::filter(.data$freq >= 20) |>
-#       dplyr::mutate(sex = "female")
+#       dplyr::mutate(database = "bar")
 #   )
 # 
-# ggPlotSunburst2(treatmentPathways = df)
+# ggPlotSunburst2(treatmentPathways = df, minFreq = 20, strataX = "database", style = "darwin")
