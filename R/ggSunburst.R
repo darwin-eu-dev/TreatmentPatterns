013e8c120482ecc66d695f46db395e23d409e8bd
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-03-10 10:17:13 +0100

    Updated docs and tests

--- R/ggSunburst.R
+++ R/ggSunburst.R
@@ -1,141 +1,210 @@
-computeXCoords <- function(table, size) {
-  table %>%
-    dplyr::group_by(.data$sex, .data$age, .data$index_year) %>%
-    dplyr::arrange(desc(.data$freq)) %>%
+mergeIndividualPathways <- function(treatmentPathways, strataX, strataY) {
+  # layerOneTotal <- sum(treatmentPathways$freq)
+  maxLayer <- max(sapply(strsplit(treatmentPathways$pathway, split = "-"), length))
+
+  layerColumns <- sprintf("layer_%s", 1:maxLayer)
+  
+  naReplaceList <- as.list(rep("", maxLayer))
+  names(naReplaceList) <- layerColumns
+
+  dat <- treatmentPathways |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY))) |>
     dplyr::mutate(
       total = sum(.data$freq),
-      frac = .data$freq / .data$total * size,
+      path_to_sep = .data$pathway,
+      path_id = dplyr::row_number()
+    ) |>
+    tidyr::separate_wider_delim(
+      cols = "path_to_sep",
+      delim = "-",
+      names = layerColumns,
+      too_few = "align_start"
+    ) |>
+    tidyr::replace_na(naReplaceList)
+
+  for (i in seq_len(length(layerColumns))) {
+    dat <- dat |>
+      dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), !!!rlang::parse_exprs(layerColumns[1:i])) |>
+      dplyr::mutate(!!rlang::sym(sprintf("l%s_freq", i)) := sum(.data$freq)) |>
+      dplyr::ungroup()
+  }
+
+  to0 <- as.list(rep(0, maxLayer))
+  names(to0) <- sprintf("l%s_freq", 1:maxLayer)
+
+  dat <- dat |>
+    tidyr::replace_na(to0) |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY))) |>
+    dplyr::arrange(!!!rlang::parse_exprs(names(to0)), !!!rlang::parse_exprs(layerColumns)) |>
+    dplyr::mutate(
+      frac = .data$freq / .data$total * 100,
       xmax = cumsum(.data$frac),
       xmin = .data$xmax - .data$frac
-    )
+    ) |>
+    tidyr::separate_longer_delim(cols = "pathway", delim = "-") |>
+    dplyr::rename(event = "pathway") |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), .data$path_id) |>
+    dplyr::mutate(layer = dplyr::row_number()) |>
+    dplyr::ungroup()
+
+  dat <- lapply(1:maxLayer, function(i) {
+    layerDat <- dat |>
+      dplyr::filter(.data$layer == i) |>
+      dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), !!!rlang::parse_exprs(layerColumns[1:i])) |>
+      dplyr::reframe(
+        layer = .data$layer,
+        event = .data$event,
+        freq = sum(.data$freq),
+        frac = sum(.data$frac),
+        xmin = min(.data$xmin),
+        xmax = max(.data$xmax)
+      ) |>
+      dplyr::distinct()
+  }) |>
+    dplyr::bind_rows()
 }
 
-splitPaths <- function(table) {
-  table %>%
-    dplyr::mutate(path_id = row_number()) %>%
-    dplyr::mutate(path = .data$pathway) %>%
-    tidyr::separate_longer_delim(cols = "pathway", delim = "-") %>%
-    dplyr::group_by(.data$sex, .data$age, .data$index_year, .data$path_id) %>%
-    dplyr::mutate(layer = row_number()) %>%
-    dplyr::ungroup() %>%
-    dplyr::rename(label = "pathway")
-}
-
-splitCombinations <- function(table, groupCombinations) {
-  if (groupCombinations) {
-    table %>%
-      dplyr::mutate(is_combo = grepl(x = .data$label, pattern = "\\+")) %>%
-      dplyr::group_by(.data$path_id, .data$layer, .data$sex, .data$age, .data$index_year) %>%
-      dplyr::mutate(
-        combo_id = dplyr::case_when(
-          .data$is_combo ~ row_number(),
-          .default = 1
-        ),
-        label = dplyr::case_when(
-          .data$is_combo ~ "Combination",
-          .default = .data$label
-        )
-      ) %>%
-      dplyr::ungroup()
-  } else {
-    table %>%
-      dplyr::mutate(is_combo = grepl(x = .data$label, pattern = "\\+")) %>%
-      dplyr::group_by(.data$path_id, .data$layer, .data$sex, .data$age, .data$index_year) %>%
-      tidyr::separate_longer_delim(cols = "label", delim = "+") %>%
-      dplyr::mutate(combo_id = dplyr::case_when(
-        .data$is_combo ~ row_number(),
-        .default = 1
-      )) %>%
-      dplyr::ungroup()
-  }
-}
-
-computeYCoords <- function(table, size) {
-  table %>%
-    dplyr::group_by(.data$path_id, .data$layer, .data$sex, .data$age, .data$index_year) %>%
-    dplyr::mutate(max_combo = max(.data$combo_id)) %>%
-    dplyr::ungroup() %>%
-    dplyr::rowwise() %>%
+splitCombinations <- function(treatmentPathways, strataX, strataY) {
+  layerCols <- names(treatmentPathways)[grepl(pattern = "^layer_\\d$", names(treatmentPathways))]
+  
+  n <- sum(grepl(names(treatmentPathways), pattern = "^layer_\\d$"))
+  
+  treatmentPathways |>
     dplyr::mutate(
-      ymin = (1 / .data$max_combo) * size * (.data$combo_id - 1) + (size * .data$layer - size) + size,
-      ymax = (1 / .data$max_combo) * size * (.data$combo_id) + (size * .data$layer - size) + size
+      event_to_split = .data$event
+    ) |>
+    tidyr::separate_longer_delim(cols = "event_to_split", delim = "+") |>
+    dplyr::group_by(!!!rlang::parse_exprs(c(strataX, strataY)), !!!rlang::parse_exprs(layerCols)) |>
+    dplyr::mutate(
+      comb_id = dplyr::row_number(),
+      comb_max = max(dplyr::row_number())
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      ymin = 1 / .data$comb_max * (comb_id - 1) + .data$layer,
+      ymax = 1 / .data$comb_max * comb_id + .data$layer
+    ) |>
+    dplyr::rename(
+      event_org = "event",
+      event = "event_to_split"
     )
 }
 
-formatDataSunburst <- function(treatmentPathways, size, groupCombinations) {
-  treatmentPathways %>%
-    computeXCoords(size) %>%
-    splitPaths() %>%
-    splitCombinations(groupCombinations) %>%
-    computeYCoords(size)
-}
+plotSunburst <- function(treatmentPathways, strataX, strataY) {
+  ggDat <- mergeIndividualPathways(treatmentPathways, strataX, strataY) |>
+    splitCombinations(strataX, strataY)
 
-makeGgSunburst <- function(data) {
-  gg <- ggplot2::ggplot(data = data)
+  gg <- ggplot2::ggplot(data = ggDat)
   
-  nLayers <- max(data$layer)
-  yMax <- max(data$ymax)
+  nLayers <- sum(grepl(pattern = "^layer_\\d$", names(ggDat)))
   
-  for (layer in seq_len(nLayers)) {
-    gg <- gg +
-      ggplot2::geom_rect(
-        data = data %>%
-          dplyr::filter(.data$layer == layer),
-        mapping = ggplot2::aes(
-          ymin = .data$ymin,
-          ymax = .data$ymax,
-          # Width
-          xmin = .data$xmin,
-          xmax = .data$xmax,
-          fill = .data$label
-        ),
-        colour = "#000000"
-      )
-  }
-  
+  for (i in 1:nLayers)
+    gg <- gg + ggplot2::geom_rect(
+      data = ggDat |>
+        dplyr::filter(.data$layer == i),
+      mapping = ggplot2::aes(
+        ymin = .data$ymin,
+        ymax = .data$ymax,
+        xmin = .data$xmin,
+        xmax = .data$xmax,
+        fill = .data$event
+      ),
+      colour = "#000000"
+    )
+
   gg +
     ggplot2::coord_polar() +
-    visOmopResults::themeVisOmop() +
     ggplot2::theme(
       axis.text.y = ggplot2::element_blank(),
       axis.ticks = ggplot2::element_blank()
     ) +
-    ggplot2::ylim(0, yMax)
+    ggplot2::ylim(0, nLayers + 1) +
+    ggplot2::facet_grid(
+      rows = vars(!!!rlang::parse_exprs(strataY)),
+      cols = vars(!!!rlang::parse_exprs(strataX))
+    )
 }
 
 #' ggSunburst
 #'
-#' @template param_treatmentPathways
-#' @template param_groupCombinations
-#' @param unit (`character(1)`) Either `"count"` or `"percent"`, to scale the plot to.
+#' ggplot2 implementation of the sunburst plot.
 #'
-#' @returns (`gg`, `ggplot`)
+#' @param treatmentPathways (`data.frame`) Treatment Pathways result containing
+#' at least a 'pathway' and 'freq' column.
+#' @param minFreq (`numeric(1)`) A minimum frequency of pathways to plot.
+#' @param strataX (`character(n)`) Column names to facet by horizontally.
+#' @param strataY (`character(n)`) Column names to facet by vertically.
+#' @param style (`character(1)`) Name of the style to use. Right now only
+#' `"darwin"` is supported. Anything else will default to the default styling.
+#' Themes can be changed by overwriting it with any of the ggplot2 theme
+#' functions (i.e. `ggSunburst(df) + ggplot2::theme_classic()`).
+#'
+#' @returns `ggplot`
+#'
 #' @export
 #'
 #' @examples
-#' # Dummy data, typically read from treatmentPathways.csv
-#' treatmentPatwhays <- data.frame(
-#'   pathway = c("Acetaminophen", "Acetaminophen-Amoxicillin+Clavulanate",
-#'            "Acetaminophen-Aspirin", "Amoxicillin+Clavulanate", "Aspirin"),
-#'   freq = c(206, 6, 14, 48, 221),
-#'   sex = rep("all", 5),
-#'   age = rep("all", 5),
-#'   index_year = rep("all", 5)
+#' # Load in dummy results
+#' tpr <- TreatmentPatterns::TreatmentPatternsResults$new(
+#' filePath = system.file(
+#'   "DummyOutput", "output.zip",
+#'   package = "TreatmentPatterns"
+#' )
 #' )
 #' 
-#' if (interactive()) {
-#'   ggSunburst(treatmentPatwhays)
-#' }
-ggSunburst <- function(treatmentPathways, groupCombinations = FALSE, unit = "percent") {
-  checkInstall("ggplot2")
-  checkInstall("visOmopResults")
+#' ggSunburst(
+#'   treatmentPathways = tpr$treatment_pathways,
+#'   minFreq = 20,
+#'   style = "darwin"
+#' )
+ggSunburst <- function(treatmentPathways, minFreq = 0, strataX = "", strataY = "", style = "default") {
+  collection <- checkmate::makeAssertCollection()
+  checkmate::assertDataFrame(x = treatmentPathways, min.cols = 2, add = collection)
+  checkmate::assertNames(x = names(treatmentPathways), must.include = c("pathway", "freq"), .var.name = "treatmentPathways", add = collection)
+  checkmate::assertIntegerish(x = minFreq, lower = 0, len = 1, add = collection)
+  checkmate::assertCharacter(x = strataX, len = 1, add = collection)
+  checkmate::assertCharacter(x = strataY, len = 1, add = collection)
+  checkmate::assertCharacter(x = style, len = 1, add = collection)
+  checkmate::reportAssertions(collection)
 
-  size <- if (unit == "percent") {
-    100
-  } else if (unit == "count") {
-    sum(treatmentPathways$freq)
+  colNames <- names(treatmentPathways)
+
+  extraCols <- colNames[!colNames %in% c("pathway", "freq")]
+
+  colGroups <- treatmentPathways |>
+    dplyr::group_by(!!!rlang::parse_exprs(extraCols)) |>
+    dplyr::reframe()
+
+  cols <- sapply(extraCols, function(col) {
+    groups <- unique(colGroups[[col]])
+    if (length(groups) > 1) {
+      sprintf("%s: %s", col, paste(sprintf("`%s`", groups), collapse = ", "))
+    }
+  })
+
+  cols <- cols[!sapply(cols, is.null)] |>
+    unlist() |>
+    as.character()
+
+  if (length(cols) > 0) {
+    warning(sprintf("Found columns with multiple groups: %s. You can pass the columns as strata in: `strataX` and/or `strataY` ", cols))
   }
 
-  formatDataSunburst(treatmentPathways, size, groupCombinations) %>%
-    makeGgSunburst()
+  gg <- treatmentPathways |>
+    dplyr::filter(.data$freq >= minFreq) |>
+    plotSunburst(strataX, strataY)
+
+  if (minFreq >= 0) {
+    nPaths <- treatmentPathways |>
+      dplyr::filter(.data$freq < minFreq) |>
+      nrow()
+    message(sprintf("Filtered out %s pathways with a frequency < %s", nPaths, minFreq))
+  }
+
+  if (style == "darwin") {
+    gg <- gg +
+      visOmopResults::themeVisOmop(style = "darwin")
+  }
+
+  return(gg)
 }
--- tests/testthat/test-ggSunburst.R
+++ tests/testthat/test-ggSunburst.R
@@ -14,45 +14,48 @@ test_that("ggSunburst", {
 
   total <- sum(treatmentPathways$freq)
   df <- treatmentPathways %>%
+    dplyr::group_by(NULL) |>
     mutate(
       frac = .data$freq / total * 100
     )
 
-  expect_true(all(unique(gg$data$frac) %in% unique(df$frac)))
+  # "A" layer 1
+  testthat::expect_identical(
+    treatmentPathways |>
+      dplyr::filter(startsWith(.data$pathway, prefix = "A")) |>
+      dplyr::reframe(freq = sum(freq)) |>
+      dplyr::pull(.data$freq),
+
+    gg$data |>
+      dplyr::filter(.data$event_org == "A", .data$layer == 1) |>
+      dplyr::pull("freq")
+  )
+
+  # "B" layer 1
+  testthat::expect_identical(
+    treatmentPathways |>
+      dplyr::filter(.data$pathway %in% c("B", "B-A-C")) |>
+      dplyr::reframe(freq = sum(freq)) |>
+      dplyr::pull(.data$freq),
+    
+    gg$data |>
+      dplyr::filter(.data$event_org == "B", .data$layer == 1) |>
+      dplyr::pull("freq")
+  )
+
+  # "B" layer 2
+  testthat::expect_identical(
+    treatmentPathways |>
+      dplyr::filter(.data$pathway %in% c("C-B", "A-B-C")) |>
+      dplyr::reframe(freq = sum(freq)) |>
+      dplyr::pull(.data$freq),
+
+    gg$data |>
+      dplyr::filter(.data$event_org == "B", .data$layer == 2) |>
+      dplyr::pull("freq") |>
+      sum()
+  )
+
+
   expect_true(any(class(gg) %in% c("gg", "ggplot")))
-})
-
-test_that("ggSunburst: groupCombinations", {
-  skip_on_cran()
-  skip_if_not_installed("ggplot2")
-
-  treatmentPathways <- data.frame(
-    pathway = c("A", "C-B", "A-B-C", "B", "B+A", "B-A-C"),
-    freq = c(100, 75, 25, 500, 350, 20),
-    age = "all",
-    sex = "all",
-    index_year = "all"
-  )
-
-  gg <- ggSunburst(treatmentPathways, groupCombinations = TRUE)
-
-  expect_true("Combination" %in% gg$data$label)
-  expect_false(all(grepl("\\+", gg$data$label)))
-})
-
-test_that("ggSunburst: unit", {
-  skip_on_cran()
-  skip_if_not_installed("ggplot2")
-
-  treatmentPathways <- data.frame(
-    pathway = c("A", "C-B", "A-B-C", "B", "B+A", "B-A-C"),
-    freq = c(100, 75, 25, 500, 350, 20),
-    age = "all",
-    sex = "all",
-    index_year = "all"
-  )
-
-  gg <- ggSunburst(treatmentPathways, unit = "count")
-
-  expect_true(all(unique(gg$data$frac) %in% unique(treatmentPathways$freq)))
 })
