efe1211b541562babe0c30ccebdf28f895e21fa6
Maarten van Kessel <m.l.vankessel@erasmusmc.nl>
2026-08-06 14:47:46 +0200

    Moved attrition to own file

--- R/attrition.R
+++ R/attrition.R
@@ -1,34 +1,58 @@
-fetchAttritionCounts <- function(andromeda, table = "treatmentHistory") {
-  n <- andromeda[[table]] %>%
-    dplyr::group_by(.data$personId) %>% 
-    dplyr::summarise(n = dplyr::n()) %>%
-    dplyr::pull()
-  list(
-    nSubjects = length(n),
-    nRecords = sum(n)
-  )
+appendAttrition <- function(tbl, andromeda, reason, reason_id) {
+  if ("list" %in% class(tbl)) {
+    tbl <- tbl |>
+      purrr::reduce(dplyr::union_all)
+  }
+  
+  attrition <- tbl |>
+    dplyr::group_by(.data$cohort_definition_id) |>
+    dplyr::summarise(
+      number_records = dplyr::n(),
+      number_subjects = dplyr::n_distinct(.data$subject_id)
+    ) |>
+    dplyr::mutate(
+      reason_id = reason_id,
+      reason = reason,
+      excluded_records = NA,
+      excluded_subjects = NA
+    ) |>
+    dplyr::collect()
+  
+  andromeda$attrition <- andromeda$attrition |>
+    dplyr::union_all(attrition, copy = TRUE) |>
+    dplyr::group_by(.data$cohort_definition_id) |>
+    dbplyr::window_order(dplyr::desc(.data$number_records)) |>
+    dplyr::mutate(
+      excluded_records = dplyr::case_when(
+        is.na(.data$excluded_records) ~ dplyr::lag(.data$number_records) - .data$number_records
+      ),
+      excluded_subjects = dplyr::case_when(
+        is.na(.data$excluded_subjects) ~ dplyr::lag(.data$number_subjects) - .data$number_subjects
+      )
+    ) |>
+    dplyr::mutate(
+      excluded_records = dplyr::case_when(
+        is.na(.data$excluded_records) ~ 0,
+        .default = .data$excluded_records
+      ),
+      excluded_subjects = dplyr::case_when(
+        is.na(.data$excluded_subjects) ~ 0,
+        .default = .data$excluded_subjects
+      )
+    )
+  
+  return(andromeda)
 }
 
 initAttrition <- function(andromeda) {
   andromeda$attrition <- data.frame(
-    number_records = numeric(0),
-    number_subjects = numeric(0),
-    reason_id = numeric(0),
+    cohort_definition_id = integer(0),
+    number_records = integer(0),
+    number_subjects = integer(0),
+    reason_id = integer(0),
     reason = character(0),
-    time_stamp = numeric(0)
+    excluded_records = integer(0),
+    excluded_subjects = integer(0)
   )
-}
-
-appendAttrition <- function(toAdd, andromeda) {
-  if (is.null(andromeda$attrition)) initAttrition(andromeda)
-  
-  toAdd$time_stamp <- as.numeric(Sys.time())
-  
-  andromeda$attrition <- dplyr::rows_append(
-    x = andromeda$attrition,
-    y = toAdd,
-    copy = TRUE
-  )
-
-  message(sprintf("-- %s\n\tRecords: %s\n\tSubjects: %s", toAdd$reason, toAdd$number_records, toAdd$number_subjects))
+  return(andromeda)
 }
--- R/fetchCohortTable.R
+++ R/fetchCohortTable.R
@@ -63,65 +63,6 @@ conInterface <- function(connectionDetails = NULL, cdm = NULL) {
   }
 }
 
-appendAttrition <- function(tbl, andromeda, reason, reason_id) {
-  if ("list" %in% class(tbl)) {
-    tbl <- tbl |>
-      purrr::reduce(dplyr::union_all)
-  }
-
-  attrition <- tbl |>
-    dplyr::group_by(.data$cohort_definition_id) |>
-    dplyr::summarise(
-      number_records = dplyr::n(),
-      number_subjects = dplyr::n_distinct(.data$subject_id)
-    ) |>
-    dplyr::mutate(
-      reason_id = reason_id,
-      reason = reason,
-      excluded_records = NA,
-      excluded_subjects = NA
-    ) |>
-    dplyr::collect()
-
-  andromeda$attrition <- andromeda$attrition |>
-    dplyr::union_all(attrition, copy = TRUE) |>
-    dplyr::group_by(.data$cohort_definition_id) |>
-    dbplyr::window_order(dplyr::desc(.data$number_records)) |>
-    dplyr::mutate(
-      excluded_records = dplyr::case_when(
-        is.na(.data$excluded_records) ~ dplyr::lag(.data$number_records) - .data$number_records
-      ),
-      excluded_subjects = dplyr::case_when(
-        is.na(.data$excluded_subjects) ~ dplyr::lag(.data$number_subjects) - .data$number_subjects
-      )
-    ) |>
-    dplyr::mutate(
-      excluded_records = dplyr::case_when(
-        is.na(.data$excluded_records) ~ 0,
-        .default = .data$excluded_records
-      ),
-      excluded_subjects = dplyr::case_when(
-        is.na(.data$excluded_subjects) ~ 0,
-        .default = .data$excluded_subjects
-      )
-    )
-
-  return(andromeda)
-}
-
-initAttrition <- function(andromeda) {
-  andromeda$attrition <- data.frame(
-    cohort_definition_id = integer(0),
-    number_records = integer(0),
-    number_subjects = integer(0),
-    reason_id = integer(0),
-    reason = character(0),
-    excluded_records = integer(0),
-    excluded_subjects = integer(0)
-  )
-  return(andromeda)
-}
-
 #' fetchCohortTable
 #'
 #' @param cdm (`cdm_reference`) A CDM reference object.
