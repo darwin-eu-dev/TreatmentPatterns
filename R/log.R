cc9dbd64aefda1666eff1cb370eb3682759013ee
Maarten van Kessel <m.l.vankessel@erasmusmc.nl>
2026-08-12 14:05:26 +0200

    Added level to logging

--- R/log.R
+++ R/log.R
@@ -1,14 +1,16 @@
 initLog <- function(andromeda) {
   andromeda$log <- data.frame(
     time = Sys.time(),
+    level = factor("INFO", levels = c("ERROR", "WARN", "INFO")),
     message = "Created log table"
   )
   return(invisible(andromeda))
 }
 
-appendLog <- function(andromeda, msg) {
+appendLog <- function(andromeda, msg, level = "INFO") {
   log <- data.frame(
     time = Sys.time(),
+    level = factor(level, levels = c("ERROR", "WARN", "INFO")),
     message = msg
   )
   
