9b446a1c54fa98310092e7b6d03fe058ff5c61f9
Maarten van Kessel <m.l.vankessel@erasmusmc.nl>
2026-09-22 13:40:43 +0200

    added plot function

--- tests/testthat/test-summariseTargetIndexToEndOfObservation.R
+++ tests/testthat/test-summariseTargetIndexToEndOfObservation.R
@@ -116,3 +116,34 @@ testthat::test_that("tableTargetIndexToEndOfObservation", {
     tableTargetIndexToEndOfObservation(result, style = "darwin")
   })
 })
+
+testthat::test_that("plotTargetIndexToEndOfObservation", {
+  skip_on_cran()
+  skip_if_not(ableToRun()$CDMC)
+  
+  outputEnv <- computePathways(
+    cohorts = .CM$cohorts,
+    cdm = .CM$cdm,
+    .CM$cohortTableName
+  )
+  
+  result <- summariseTargetIndexToEndOfObservation(outputEnv, minCellCount = 1)
+
+  testthat::expect_no_error({
+    plotTargetIndexToEndOfObservation(result)
+  })
+  
+  testthat::expect_no_error({
+    plotTargetIndexToEndOfObservation(result, style = "darwin")
+  })
+  
+  result <- summariseTargetIndexToEndOfObservation(outputEnv, minCellCount = 1000)
+  
+  testthat::expect_no_error({
+    plotTargetIndexToEndOfObservation(result)
+  })
+  
+  testthat::expect_no_error({
+    plotTargetIndexToEndOfObservation(result, style = "darwin")
+  })
+})
