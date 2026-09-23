a39c5db7d7f090a7f2a4de0ebee78a91f1d93bd2
Maarten van Kessel <m.l.vankessel@erasmusmc.nl>
2026-09-23 12:04:41 +0200

    added tableTreatmentPathways

---
+++ tests/testthat/test-tableTreatmentPathways.R
@@ -0,0 +1,25 @@
+testthat::test_that("tableTreatmentPathways", {
+  skip_on_cran()
+  skip_if_not(ableToRun()$CDMC)
+
+  outputEnv <- computePathways(
+    cohorts = .CM$cohorts,
+    cdm = .CM$cdm,
+    .CM$cohortTableName
+  )
+
+  result <- TreatmentPatterns::export(outputEnv)
+
+  testthat::expect_no_error(
+    tableTreatmentPathways(result)
+  )
+
+  testthat::expect_no_error(
+    tableTreatmentPathways(result, style = "darwin")
+  )
+
+  result$summary_event_duration <- NULL
+  testthat::expect_error(
+    tableTreatmentPathways(result)
+  )
+})
