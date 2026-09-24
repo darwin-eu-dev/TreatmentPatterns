61f1f3729f0118630ed619448dfe0cef89c0a6f0
Maarten van Kessel <m.l.vankessel@erasmusmc.nl>
2026-09-24 11:42:00 +0200

    added tests

---
+++ tests/testthat/test-setCohortTypes.R
@@ -0,0 +1,87 @@
+testthat::test_that("CohortSet", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(ableToRun()$CDMC)
+
+  cohortSet <- CDMConnector::readCohortSet(
+    path = system.file(package = "TreatmentPatterns", "exampleCohorts")
+  )
+
+  testthat::expect_error(
+    setCohortTypes(cohortSet)
+  )
+
+  testthat::expect_error(
+    setCohortTypes(cohortSet, "event")
+  )
+
+  testthat::expect_no_error(
+    setCohortTypes(cohortSet, c(rep("event", 7), "target"))
+  )
+})
+
+testthat::test_that("CohortsToCreate-CohortsGenerated", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(ableToRun()$CG)
+
+  connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+  
+  cohortsToCreate <- CohortGenerator::createEmptyCohortDefinitionSet()
+  
+  cohortJsonFiles <- list.files(path = system.file("exampleCohorts", package = "TreatmentPatterns"), full.names = TRUE)
+  
+  for (i in 1:length(cohortJsonFiles)) {
+    cohortJsonFileName <- cohortJsonFiles[i]
+    cohortName <- tools::file_path_sans_ext(basename(cohortJsonFileName))
+    cohortJson <- readChar(cohortJsonFileName, file.info(cohortJsonFileName)$size)
+    cohortExpression <- CirceR::cohortExpressionFromJson(cohortJson)
+    cohortSql <- CirceR::buildCohortQuery(cohortExpression, options = CirceR::createGenerateOptions(generateStats = FALSE))
+    cohortsToCreate <- data.frame(
+      cohortId = i,
+      cohortName = cohortName, 
+      sql = cohortSql,
+      stringsAsFactors = FALSE
+    ) |>
+      rbind(cohortsToCreate)
+  }
+  
+  cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = "my_cohort_table")
+  CohortGenerator::createCohortTables(
+    connectionDetails = connectionDetails,
+    cohortDatabaseSchema = "main",
+    cohortTableNames = cohortTableNames
+  )
+  
+  cohortsGenerated <- CohortGenerator::generateCohortSet(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = "main",
+    cohortDatabaseSchema = "main",
+    cohortTableNames = cohortTableNames,
+    cohortDefinitionSet = cohortsToCreate
+  )
+
+  # cohortsToCreate
+  testthat::expect_error(
+    setCohortTypes(cohortsToCreate)
+  )
+  
+  testthat::expect_error(
+    setCohortTypes(cohortsToCreate, "event")
+  )
+  
+  testthat::expect_no_error(
+    setCohortTypes(cohortsToCreate, c(rep("event", 7), "target"))
+  )
+
+  # cohortsGenerated
+  testthat::expect_error(
+    setCohortTypes(cohortsGenerated)
+  )
+  
+  testthat::expect_error(
+    setCohortTypes(cohortsGenerated, "event")
+  )
+  
+  testthat::expect_no_error(
+    setCohortTypes(cohortsGenerated, c(rep("event", 7), "target"))
+  )
+})
